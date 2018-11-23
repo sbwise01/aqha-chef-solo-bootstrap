b12c265034fff6706c0c77ffd3bf3935138878e0
Brad Wise <brad@foghornconsulting.com>
2018-11-23 13:53:47 -0600

    just use packer chef cookbooks location

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 default['aqha']['install_dir'] = "/opt/aqha-chef-solo-bootstrap"
-default['aqha']['cookbooks_dir']="/tmp/aqha-chef-solo-bootstrap/cookbooks"
+default['aqha']['cookbooks_dir']="/tmp/packer-chef-solo/cookbooks-0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -24,7 +24,6 @@ template "#{node['aqha']['install_dir']}/bootstrap.sh" do
   group 'root'
   variables(
     :install_dir => node['aqha']['install_dir'],
-    :cookbooks_dir => node['aqha']['cookbooks_dir'],
     :bootstrap_environment_name => node['aqha']['bootstrap_environment_name']
   )
 end
@@ -35,7 +34,8 @@ template "#{node['aqha']['install_dir']}/solo.rb" do
   owner 'root'
   group 'root'
   variables(
-    :install_dir => node['aqha']['install_dir']
+    :install_dir => node['aqha']['install_dir'],
+    :cookbooks_dir => node['aqha']['cookbooks_dir']
   )
 end
 
